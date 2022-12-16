5134447dee20cfb4daf2104c3215023add8f8fab
pythagoras58 <solomondanso58@gmail.com>
2022-12-16 22:18:27 +0000

    List

--- src/ChallengeOne/ChallengeOne.java
+++ src/ChallengeOne/ChallengeOne.java
@@ -1,7 +1,14 @@
 package ChallengeOne;
 
+import java.util.List;
+
 public class ChallengeOne {
     public static void main(String[] args) {
-        
+       int[] arr = {1,3,5,7,9};
+
+    }
+
+    public static void miniMaxValue(List<Integer> arr){
+
     }
 }
