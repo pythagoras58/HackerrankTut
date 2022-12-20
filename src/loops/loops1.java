96add1d742ad99ad4778332019cae1b7f7139b8b
pythagoras58 <solomondanso58@gmail.com>
2022-12-20 20:17:08 +0000

    Looops 3

--- src/loops/loops1.java
+++ src/loops/loops1.java
@@ -1,7 +1,21 @@
 package loops;
 
-public class loops1 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class loops1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        if((N>=2)&&(N<=20)){
+            for(int i=1; i<=10; i++){
+                int v = N*i;
+                System.out.println(N + " x " + i + " = " + v);
+            }
+        }
+
+        bufferedReader.close();
     }
 }
