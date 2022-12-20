20e716f24b50bf00895f016c429e1a5796f8b783
pythagoras58 <solomondanso58@gmail.com>
2022-12-20 18:22:47 +0000

    Formatters 1

--- src/readers/readers.java
+++ src/readers/readers.java
@@ -9,7 +9,7 @@ public class readers {
         int i = scan.nextInt();
         double d = scan.nextDouble();
         scan.nextLine();
-        String s = scan.next();
+        String s = scan.nextLine();
 
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
