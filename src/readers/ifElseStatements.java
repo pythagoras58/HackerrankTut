6ecf9f3621c2cc9996aceff6769e7ede4e64a344
pythagoras58 <solomondanso58@gmail.com>
2022-12-20 17:32:22 +0000

    Statement 2

--- src/readers/ifElseStatements.java
+++ src/readers/ifElseStatements.java
@@ -1,4 +1,12 @@
 package readers;
 
+import java.util.Scanner;
+
 public class ifElseStatements {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
 }
