60501a6426e85cbb1d3e9ee3997ae80cf09f49a5
pythagoras58 <solomondanso58@gmail.com>
2022-12-20 18:16:46 +0000

    Readers 5

--- src/readers/readers.java
+++ src/readers/readers.java
@@ -6,6 +6,14 @@ public class readers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.next();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
 
         scan.close();
     }
